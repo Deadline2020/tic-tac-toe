6a9e02b43b9dedf9316adb35c644f946876a28d1
Andrew <Deadline2020@yandex.by>
2019-10-07 00:20:46 +0300

    feat: add solution task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,48 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.currentPlayerSymbol = true;
+    this.grid = [null, null, null, null, null, null, null, null, null];
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.currentPlayerSymbol ? "x" : "o";
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (!this.grid[3 * columnIndex + rowIndex]) {
+      this.grid[3 * columnIndex + rowIndex] = this.currentPlayerSymbol ? "x" : "o";
+      this.currentPlayerSymbol = !this.currentPlayerSymbol;
     }
+  }
 
-    getCurrentPlayerSymbol() {
+  isFinished() {
+    if (this.isDraw() || this.getWinner()) return true;
+    return false;
+  }
 
+  getWinner() {
+    const winComb = [[6, 7, 8], [3, 4, 5], [0, 1, 2], [6, 3, 0], [7, 4, 1], [8, 5, 2], [6, 4, 2], [8, 4, 0]];
+    for (let comb of winComb) {
+      if (this.grid[comb[0]] === this.grid[comb[1]] && this.grid[comb[1]] === this.grid[comb[2]]) {
+        return this.grid[comb[0]];
+      }
     }
+    return null;
+  }
 
-    nextTurn(rowIndex, columnIndex) {
+  noMoreTurns() {
+    if (~this.grid.indexOf(null)) return false;
+    return true;
+  }
 
-    }
+  isDraw() {
+    if (this.noMoreTurns() && !this.getWinner()) return true;
+    return false;
+  }
 
-    isFinished() {
-
-    }
-
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.grid[3 * colIndex + rowIndex];
+  }
 }
 
 module.exports = TicTacToe;
